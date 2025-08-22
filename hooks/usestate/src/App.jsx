304f5c117aa274c23b5da627d369e76b061b38a5
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-22 10:39:28 -0300

    useState

---
+++ hooks/usestate/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Produto } from "./Produto";
+
+export default function App() {
+  const [dados, setDados] = useState(null);
+  const [carregando, setCarregando] = useState(null);
+
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
+
+  return (
+    <div>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        notebook
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        smartphone
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        tablet
+      </button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
+    </div>
+  )
+}
