e6c77fefe6bc2ca769b8d76da522f80cfa5170f4
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-25 19:15:35 -0300

    feat: utilizando o package react-router-dom

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -1,10 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
+
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+
 export default function App() {
-
   return (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-  )
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
-
-
