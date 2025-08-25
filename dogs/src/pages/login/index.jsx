6fc65646a63f458bc29c5b49f8347d9b5159cce0
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-25 19:33:39 -0300

    feat: criação de componentes de login e routes

--- dogs/src/pages/login/index.jsx
+++ dogs/src/pages/login/index.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { LoginForm } from "./login-form";
+import { LoginCreate } from "./login-create";
+import { LoginPasswordLost } from "./login-password-lost";
+import { LoginPasswordReset } from "./login-password-reset";
+
 export function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 }
