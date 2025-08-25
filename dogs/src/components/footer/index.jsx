4670e918f6475a6c4d99fa0be177d8e50ffb7fbb
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-25 19:08:28 -0300

    feat: criando components globais

---
+++ dogs/src/components/footer/index.jsx
@@ -0,0 +1,6 @@
+import styles from "./footer.module.css";
+
+export function Footer() {
+  return <div className={styles.footer}>Footer</div>;
+}
+
---
+++ dogs/src/components/header/index.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./header.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <nav className="container">
+        <Link to="/" end>
+          Home
+        </Link>
+        <Link to="login" end>
+          Login / Criar
+        </Link>
+      </nav>
+    </div>
+  );
+}
