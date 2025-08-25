885a3cb10d957819e79ee1f80a1f832a911fa8cc
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-25 12:34:07 -0300

    desafio router dom

---
+++ react-router/desafio-router/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Produtos from "./Components/Produtos";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Contato from "./Components/Contato";
+import Produto from "./Components/Produto";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ react-router/desafio-router/src/Components/Produto.jsx
@@ -0,0 +1,46 @@
+import styles from "./Produto.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Produto = () => {
+  const [produto, setProduto] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchProduto(url) {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduto(json);
+      } catch (erro) {
+        setError("Um erro ocorreu", console.log(erro));
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
+  }, [id]);
+
+  if (loading) return <div className="loading"></div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
+  return (
+    <section className={styles.produto + " animeLeft"}>
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={styles.preco}>R$ {produto.preco}</span>
+        <p className={styles.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Produto;
