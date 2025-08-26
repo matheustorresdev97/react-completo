e69100b5f8f7143b191bbf59e6a41a72c0ea41c2
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-26 08:31:07 -0300

    feat: estrutura e estilizacao da secao de coments

---
+++ dogs/src/components/photos-comments-form/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { COMMENT_POST } from "../../api";
+import { Enviar } from "../../assets/enviar.svg?react";
+
+import { Error } from "../helper/error";
+import styles from "./photos-comments-form.module.css";
+
+export function PhotoCommentsForm({ id, setComments }) {
+  const [comment, setComment] = useState("");
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const token = window.localStorage.getItem("token");
+    const { url, options } = COMMENT_POST(id, { comment }, token);
+    const { response, json } = await request(url, options);
+
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea
+        className={styles.textarea}
+        id={comment}
+        name={comment}
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+}
