502ea57153b5010e65783beda21f1400d21e6645
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-25 21:21:06 -0300

    feat: criando tela ed cadastro e estilizado

--- dogs/src/pages/login/login-create.jsx
+++ dogs/src/pages/login/login-create.jsx
@@ -1,3 +1,39 @@
+import { useContext } from "react";
+import { useForm } from "../../hooks/useForm";
+
+import { UserContext } from "../../user-context";
+
+import { Input } from "../../components/forms/input";
+import { Button } from "../../components/forms/button";
+
 export function LoginCreate() {
-  return <div>Login Criar</div>;
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+
+  const { userLogin } = useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="E-mail" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 }
