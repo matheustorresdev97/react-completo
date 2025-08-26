46e5ff13ae6824c42033fcb58f3e082b6a29b2be
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-25 21:14:12 -0300

    feat: estilizacao tela login

--- dogs/src/pages/login/index.jsx
+++ dogs/src/pages/login/index.jsx
@@ -1,14 +1,17 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { UserContext } from "../../user-context";
+import styles from "./login.module.css";
 
 export function Login() {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Outlet />
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Outlet />
+      </div>
+    </section>
   );
 }
