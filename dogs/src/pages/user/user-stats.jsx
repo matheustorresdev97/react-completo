8f9915458164b6820b2ec8c036ece9659039fee4
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-26 19:56:11 -0300

    feat: pagina de estatisticas

--- dogs/src/pages/user/user-stats.jsx
+++ dogs/src/pages/user/user-stats.jsx
@@ -1,4 +1,33 @@
+import Head from "../Helper/Head";
+import { useFetch } from "../../hooks/useFetch";
+import { STATS_GET } from "../../api";
+import { Loading } from "../../components/helper/loading";
+import { Error } from "../../components/helper/error";
+import { lazy, Suspense, useEffect } from "react";
+const UserStatsGraphs = lazy(() =>
+  import("../../components/user-stats-graphs")
+);
+
 export default function UserStats() {
-  return <div>Estatisticas</div>;
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </Suspense>
+    );
+  else return null;
 }
-    
